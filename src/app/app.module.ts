a3c31b44c5502c12ba9c586888d45ac0ea0920df
puhinetsn <puhinetsn82@gmail.com>
2024-07-10 13:36:54 +0300

    created: login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskLoginComponent } from './tasks/task-login/task-login.component';
-import { TaskSignupComponent } from './tasks/task-signup/task-signup.component';
 import { TaskMainComponent } from './tasks/task-main/task-main.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatRippleModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskLoginComponent,
-    TaskSignupComponent,
     TaskMainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRippleModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   providers: [
     provideClientHydration(),
--- src/app/tasks/task-main/task-main.component.ts
+++ src/app/tasks/task-main/task-main.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task-main',
   templateUrl: './task-main.component.html',
-  styleUrl: './task-main.component.css'
+  styleUrls: ['./task-main.component.css'] 
 })
 export class TaskMainComponent {
-
+  
 }
